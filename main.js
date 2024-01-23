7f3573a132d5b12647ca8ebf7e347ffed6f224cc
Angel <angelortizsanchez383@gmail.com>
2024-01-22 16:37:02 -0800

    after a long time in school and learning api functionality I pulled a weather array

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import "./styles/Style.css"
+import { getWeather } from "./weather"
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(
+    e => {
+        console.error(e)
+        alert("Error getting Weather.")
+    }
+)
