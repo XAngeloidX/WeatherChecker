c4ab390622f6617c5c875c6a4697aebdb43d05ee
Angel <angelortizsanchez383@gmail.com>
2024-01-22 18:18:07 -0800

    One last commit the cloud-sun had to be renamed and I changed its color

--- iconMap.js
+++ iconMap.js
@@ -1,7 +1,7 @@
 export const ICON_MAP = new Map()
 
 addMapping([0,1], "sun")
-addMapping([2], "cloud-sun")
+addMapping([2], "sunCloud")
 addMapping([3], "cloudy")
 addMapping([45,48], "fog")
 addMapping([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "rain")
