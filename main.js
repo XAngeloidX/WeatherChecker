07021322511f1e02cb066254641b68208c29f886
Angel <angelortizsanchez383@gmail.com>
2024-02-15 17:31:05 -0800

    Uploading Weather App to Github

--- main.js
+++ main.js
@@ -1,83 +1,88 @@
-import "./styles/Style.css"
-import { getWeather } from "./weather"
-import { ICON_MAP } from "./iconMap"
+import "./styles/Style.css";
+import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMap";
 
-navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+navigator.geolocation.getCurrentPosition(
+  coordinatesDetected,
+  coordinatesFailed
+);
 
-function positionSuccess({ coords }) {
+function coordinatesDetected({ coords }) {
   getWeather(
     coords.latitude,
     coords.longitude,
     Intl.DateTimeFormat().resolvedOptions().timeZone
   )
-    .then(renderWeather)
-    .catch(e => {
-      console.error(e)
-      alert("Error getting weather.")
-    })
+    .then(weatherInfo)
+    .catch((e) => {
+      console.error(e);
+      alert("Error getting weather.");
+    });
 }
 
-function positionError() {
+function coordinatesFailed() {
   alert(
-    "There was an error getting your location. Please allow us to use your location and refresh the page."
-  )
+    "There was an error retrieving your devices position. Please refresh and try again."
+  );
 }
 
-function renderWeather({ current, daily, hourly }) {
-  renderCurrentWeather(current)
-  renderDailyWeather(daily)
-  renderHourlyWeather(hourly)
-  document.body.classList.remove("blurryStart")
+function weatherInfo({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
+  document.body.classList.remove("blurryStart");
 }
 
 function setValue(selector, value, { parent = document } = {}) {
-    parent.querySelector(`[data-${selector}]`).textContent = value
-  }
-  
-  function getIconUrl(iconCode) {
-    return `icons/${ICON_MAP.get(iconCode)}.svg`
-  }
-  
-  const currentIcon = document.querySelector("[data-current-icon]")
-  function renderCurrentWeather(current) {
-    currentIcon.src = getIconUrl(current.iconCode)
-    setValue("current-temp", current.currentTemp)
-    setValue("current-high", current.highTemp)
-    setValue("current-low", current.lowTemp)
-    setValue("current-fl-high", current.highFeelsLike)
-    setValue("current-fl-low", current.lowFeelsLike)
-    setValue("current-wind", current.windSpeed)
-    setValue("current-precip", current.precip)
-  }
-
-const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
-const dailySection = document.querySelector("[data-day-section]")
-const dayCardTemplate = document.getElementById("day-card-template")
-function renderDailyWeather(daily) {
-  dailySection.innerHTML = ""
-  daily.forEach(day => {
-    const element = dayCardTemplate.content.cloneNode(true)
-    setValue("temp", day.maxTemp, { parent: element })
-    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element })
-    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
-    dailySection.append(element)
-  })
+  parent.querySelector(`[data-${selector}]`).textContent = value;
 }
 
-const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
-const hourlySection = document.querySelector("[data-hour-section]")
-const hourRowTemplate = document.getElementById("hour-row-template")
+function getIconUrl(iconCode) {
+  return `icons/${ICON_MAP.get(iconCode)}.svg`;
+}
+
+const currentIcon = document.querySelector("[data-current-icon]");
+function renderCurrentWeather(current) {
+  currentIcon.src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
+}
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" });
+const hourlySection = document.querySelector("[data-hour-section]");
+const hourRowTemplate = document.getElementById("hour-row-template");
 function renderHourlyWeather(hourly) {
-  hourlySection.innerHTML = ""
-  hourly.forEach(hour => {
-    const element = hourRowTemplate.content.cloneNode(true)
-    setValue("temp", hour.temp, { parent: element })
-    setValue("fl-temp", hour.feelsLike, { parent: element })
-    setValue("wind", hour.windSpeed, { parent: element })
-    setValue("precip", hour.precip, { parent: element })
-    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element })
-    setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element })
-    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
-    hourlySection.append(element)
-  })
+  hourlySection.innerHTML = "";
+  hourly.forEach((hour) => {
+    const element = hourRowTemplate.content.cloneNode(true);
+    setValue("temp", hour.temp, { parent: element });
+    setValue("fl-temp", hour.feelsLike, { parent: element });
+    setValue("wind", hour.windSpeed, { parent: element });
+    setValue("precip", hour.precip, { parent: element });
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element });
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), {
+      parent: element,
+    });
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    hourlySection.append(element);
+  });
 }
